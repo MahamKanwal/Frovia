d34b0ad796fabcdb34b1274ce7ff9a5c5b91966e
Maham Kanwal <kanwalmahsm@gmail.com>
2025-09-11 08:08:57 -0700

    added js

--- app.js
+++ app.js
@@ -4,3 +4,9 @@
 // }
 // findWord(["on","of","oh"],"off");
 
+// const palindromeWords = (word) => {
+// let words = word.reverse();
+// console.log(words);
+// }
+// let userInput = prompt("Enter a palindrome word")
+// console.log(palindromeWords(userInput));
