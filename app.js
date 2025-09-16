a7ebe9dbe435fda064e663cefcd9d27df1b823a4
Maham Kanwal <kanwalmahsm@gmail.com>
2025-09-16 09:30:12 -0700

    added js

--- app.js
+++ app.js
@@ -22,13 +22,34 @@
 // console.log(counter());
 // console.log(counter());
 
+const modelForm = document.getElementById("modelForm");
+const loginBtn = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signupBtn");
+const loginForm = document.getElementById("loginForm");
+const signupForm = document.getElementById("signupForm");
 const menu = document.getElementById("menu");
 const barBtn = document.getElementById("barBtn");
+
+const toggleFormModel = () => {
+!modelForm.classList.remove("hidden");
+}
 
 const toggleMenu = () => {
     menu.classList.toggle("h-0");
     menu.classList.toggle("h-[110px]");
 };
 
+const openSignupForm = () => {
+loginForm.classList.add("hidden");
+signupForm.classList.remove("hidden");
+}
 
+const openLoginForm = () => {
+loginForm.classList.remove("hidden");
+signupForm.classList.add("hidden");
+}
+
+
+loginBtn.addEventListener("click",openLoginForm);
+signupBtn.addEventListener("click",openSignupForm);
 barBtn.addEventListener("click",toggleMenu);
